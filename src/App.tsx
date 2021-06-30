679ef9fd6e39f49b9e009d79d785486494eef477
LeandrodeLimaC <leandrolima7472@gmail.com>
2021-06-30 01:49:46 -0300

    Better names, fix equal bug

--- src/App.tsx
+++ src/App.tsx
@@ -1,44 +1,60 @@
-import { useState } from "react";
+import { useEffect, useState } from "react";
+
+type OperatorType = {
+  [key: string]: (a: number, b: number) => number
+}
+
+const operatorsTable: OperatorType = {
+  sum: (a, b) => a + b
+}
 
 function App() {
-  const [firstNumber, setfirstNumber] = useState<string>('')
+  const [firstNumber, setFirstNumber] = useState<string>('')
   const [secondNumber, setSecondNumber] = useState<string>('')
-  const [flag, setFlag] = useState<string>('')
+  const [operatorName, setOperatorName] = useState<string>('')
   const [result, setResult] = useState<string>()
 
-  type OperatorType = {
-    [key: string]: (a: number, b: number) => number
-  }
-
-  const operatorsTable: OperatorType = {
-    sum: (a, b) => a + b
-  }
+  // useEffect(() => {
+  //   console.log('firstNumber', firstNumber)
+  //   console.log('secondNumber', secondNumber)
+  //   console.log('operatorName', operatorName)
+  //   console.log('result', result)
+  // }, [firstNumber, secondNumber, operatorName, result])
 
   function setNumber(entry: number) {
-    if (!flag) {
-      let tmp = `${firstNumber || ''}${entry}`
-      return setfirstNumber(tmp)
-    }
+    if (!operatorName)
+      return setFirstNumber(`${firstNumber || ''}${entry}`)
 
-    let tmp = `${secondNumber || ''}${entry}`
-    return setSecondNumber(tmp)
+    return setSecondNumber(`${secondNumber || ''}${entry}`)
   }
 
-  function flagaa(operation: string) {
-    if (flag)
-      getResult()
-    else
-      setFlag(operation)
+  function handleOperator(operation: string) {
+    if (operatorName) getResult()
+    else {
+      if (!firstNumber) {
+        setFirstNumber(`${result}`)
+        setResult('')
+      }
+      setOperatorName(operation)
+    }
   }
 
   function getResult() {
-    const operator: Function = operatorsTable[flag]
+    if (!operatorName) {
+      if (firstNumber) return setResult(firstNumber)
+      else return
+    }
 
+    const operator: Function = operatorsTable[operatorName]
     const result = operator(Number(firstNumber), Number(secondNumber || firstNumber))
-    setResult(`${result}`)
 
-    setFlag('')
-    setfirstNumber('')
+    setResult(`${result}`)
+    resetStates()
+  }
+
+  function resetStates() {
+    setOperatorName('')
+    setFirstNumber('')
     setSecondNumber('')
   }
 
@@ -46,14 +62,14 @@ function App() {
     <div>
       <p> first: {firstNumber} </p>
       <p> second: {secondNumber} </p>
-      <p> result: {result} </p>
+      {result && (<p> result: {result} </p>)}
       <button onClick={() => setNumber(1)}>1</button>
       <button onClick={() => setNumber(2)}>2</button>
       <button onClick={() => setNumber(3)}>3</button>
       <button onClick={() => setNumber(4)}>4</button>
       <button onClick={() => setNumber(5)}>5</button>
 
-      <button onClick={() => flagaa('sum')}>+</button>
+      <button onClick={() => handleOperator('sum')}>+</button>
       <button onClick={() => getResult()}>=</button>
     </div>
   );
